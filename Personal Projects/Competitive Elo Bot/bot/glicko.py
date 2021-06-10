c023d27baab31f7d8a4d6b210e86d25ddfa32e1e
alons3253 <71347068+alons3253@users.noreply.github.com>
2021-06-10 02:13:43 -0500

    Update glicko.py

--- Personal Projects/Competitive Elo Bot/bot/glicko.py
+++ Personal Projects/Competitive Elo Bot/bot/glicko.py
@@ -1,150 +1,90 @@
 import math
 
-WIN = 1.
-LOSS = 0.
 
-MU = 1200
-PHI = 350
-SIGMA = 0.06
-TAU = 1.0
-EPSILON = 0.000001
-Q = math.log(10) / 400
+def _g(rd):
+    return 1 / math.sqrt(1 + 3 * math.pow(rd, 2) / math.pow(math.pi, 2))
 
 
-class Rating(object):
+class Player:
 
-    def __init__(self, mu=MU, phi=PHI, sigma=SIGMA):
-        self.mu = mu
-        self.phi = phi
-        self.sigma = sigma
+    def get_rating(self):
+        return (self.__rating * 173.7178) + 1500
 
-    def __repr__(self):
-        c = type(self)
-        args = (c.__module__, c.__name__, self.mu, self.phi, self.sigma)
-        return '%s.%s(mu=%.3f, phi=%.3f, sigma=%.3f)' % args
+    def get_rd(self):
+        return self._rd * 173.7178
 
+    def __init__(self, rating=None, rd=None, vol=0.06, tau=0.5):
+        self.__rating = (rating - 1500) / 173.7178
+        self._rd = rd / 173.7178
+        self.vol = vol
+        self._tau = tau
 
-def reduce_impact(rating):
-    """The original form is `g(RD)`. This function reduces the impact of
-    games as a function of an opponent's RD.
-    """
-    return 1 / math.sqrt(1 + (3 * rating.phi ** 2) / (math.pi ** 2))
+    def _pre_rating_rd(self):
+        self._rd = math.sqrt(math.pow(self._rd, 2) + math.pow(self.vol, 2))
 
+    def update_player(self, rating_list, rd_list, outcome_list):
+        rating_list = [(x - 1500) / 173.7178 for x in rating_list]
+        rd_list = [x / 173.7178 for x in rd_list]
 
-def expect_score(rating, other_rating, impact):
-    return 1. / (1 + math.exp(-impact * (rating.mu - other_rating.mu)))
+        v = self._v(rating_list, rd_list)
+        self.vol = self._new_vol(rating_list, rd_list, outcome_list, v)
+        self._pre_rating_rd()
 
+        self._rd = 1 / math.sqrt((1 / math.pow(self._rd, 2)) + (1 / v))
 
-class Glicko2(object):
+        temp_sum = 0
+        for i in range(len(rating_list)):
+            temp_sum += _g(rd_list[i]) * (outcome_list[i] - self._e(rating_list[i], rd_list[i]))
+        self.__rating += math.pow(self._rd, 2) * temp_sum
 
-    def __init__(self, mu=MU, phi=PHI, sigma=SIGMA, tau=TAU, epsilon=EPSILON):
-        self.mu = mu
-        self.phi = phi
-        self.sigma = sigma
-        self.tau = tau
-        self.epsilon = epsilon
+    def _new_vol(self, rating_list, rd_list, outcome_list, v):
+        a_vol = math.log(self.vol**2)
+        eps = 0.000001
+        a = a_vol
 
-    def create_rating(self, mu=None, phi=None, sigma=None):
-        if mu is None:
-            mu = self.mu
-        if phi is None:
-            phi = self.phi
-        if sigma is None:
-            sigma = self.sigma
-        return Rating(mu, phi, sigma)
-
-    def scale_down(self, rating, ratio=173.7178):
-        mu = (rating.mu - self.mu) / ratio
-        phi = rating.phi / ratio
-        return self.create_rating(mu, phi, rating.sigma)
-
-    def scale_up(self, rating, ratio=173.7178):
-        mu = rating.mu * ratio + self.mu
-        phi = rating.phi * ratio
-        return self.create_rating(mu, phi, rating.sigma)
-
-    def determine_sigma(self, rating, difference, variance):
-        """Determines new sigma."""
-        phi = rating.phi
-        difference_squared = difference ** 2
-        # 1. Let a = ln(s^2), and define f(x)
-        alpha = math.log(rating.sigma ** 2)
-
-        def f(x):
-            """This function is twice the conditional log-posterior density of
-            phi, and is the optimality criterion.
-            """
-            tmp = phi ** 2 + variance + math.exp(x)
-            _a = math.exp(x) * (difference_squared - tmp) / (2 * tmp ** 2)
-            _b = (x - _a) / (self.tau ** 2)
-            return _a - _b
-        # 2. Set the initial values of the iterative algorithm.
-        a = alpha
-        if difference_squared > phi ** 2 + variance:
-            b = math.log(difference_squared - phi ** 2 - variance)
+        delta = self._delta(rating_list, rd_list, outcome_list, v)
+        tau = self._tau
+        if (delta ** 2) > ((self._rd ** 2) + v):
+            b = math.log(delta ** 2 - self._rd ** 2 - v)
         else:
             k = 1
-            while f(alpha - k * math.sqrt(self.tau ** 2)) < 0:
+            while self._f(a_vol - k * math.sqrt(tau**2), delta, v, a_vol) < 0:
                 k += 1
-            b = alpha - k * math.sqrt(self.tau ** 2)
-        # 3. Let fA = f(A) and f(B) = f(B)
-        f_a, f_b = f(a), f(b)
-        # 4. While |B-A| > e, carry out the following steps.
-        # (a) Let C = A + (A - B)fA / (fB-fA), and let fC = f(C).
-        # (b) If fCfB < 0, then set A <- B and fA <- fB; otherwise, just set
-        #     fA <- fA/2.
-        # (c) Set B <- C and fB <- fC.
-        # (d) Stop if |B-A| <= e. Repeat the above three steps otherwise.
-        while abs(b - a) > self.epsilon:
-            c = a + (a - b) * f_a / (f_b - f_a)
-            f_c = f(c)
+            b = a_vol - k * math.sqrt(tau**2)
+
+        f_a = self._f(a, delta, v, a_vol)
+        f_b = self._f(b, delta, v, a_vol)
+
+        while math.fabs(b - a) > eps:
+            c = a + ((a - b) * f_a)/(f_b - f_a)
+            f_c = self._f(c, delta, v, a_vol)
             if f_c * f_b < 0:
-                a, f_a = b, f_b
+                a = b
+                f_a = f_b
             else:
-                f_a /= 2
-            b, f_b = c, f_c
-        # 5. Once |B-A| <= e, set s' <- e^(A/2)
-        return math.exp(1) ** (a / 2)
+                f_a = f_a/2.0
+            b = c
+            f_b = f_c
+        return math.exp(a / 2)
 
-    def rate(self, rating, series):
-        # Step 2. For each player, convert the rating and RD's onto the
-        #         Glicko-2 scale.
-        rating = self.scale_down(rating)
-        # Step 3. Compute the quantity v. This is the estimated variance of the
-        #         team's/player's rating based only on game outcomes.
-        # Step 4. Compute the quantity difference, the estimated improvement in
-        #         rating by comparing the pre-period rating to the performance
-        #         rating based only on game outcomes.
-        d_square_inv = 0
-        variance_inv = 0
-        difference = 0
-        if not series:
-            # If the team didn't play in the series, do only Step 6
-            phi_star = math.sqrt(rating.phi ** 2 + rating.sigma ** 2)
-            return self.scale_up(self.create_rating(rating.mu, phi_star, rating.sigma))
-        for actual_score, other_rating in series:
-            other_rating = self.scale_down(other_rating)
-            impact = reduce_impact(other_rating)
-            expected_score = expect_score(rating, other_rating, impact)
-            variance_inv += impact ** 2 * expected_score * (1 - expected_score)
-            difference += impact * (actual_score - expected_score)
-            d_square_inv += (expected_score * (1 - expected_score) * (Q ** 2) * (impact ** 2))
-        difference /= variance_inv
-        variance = 1. / variance_inv
-        denom = rating.phi ** -2 + d_square_inv
-        phi = math.sqrt(1 / denom)
-        # Step 5. Determine the new value, Sigma', not the sigma. This
-        #         computation requires iteration.
-        sigma = self.determine_sigma(rating, difference, variance)
-        # Step 6. Update the rating deviation to the new pre-rating period
-        #         value, Phi*.
-        phi_star = math.sqrt(phi ** 2 + sigma ** 2)
-        # Step 7. Update the rating and RD to the new values, Mu' and Phi'.
-        phi = 1 / math.sqrt(1 / phi_star ** 2 + 1 / variance)
-        mu = rating.mu + phi ** 2 * (difference / variance)
-        # Step 8. Convert ratings and RD's back to original scale.
-        return self.scale_up(self.create_rating(mu, phi, sigma))
+    def _f(self, x, delta, v, a):
+        ex = math.exp(x)
+        num1 = ex * (delta**2 - self.__rating**2 - v - ex)
+        denom1 = 2 * ((self.__rating**2 + v + ex)**2)
+        return (num1 / denom1) - ((x - a) / (self._tau**2))
 
-    def rate_1vs1(self, rating1, rating2):
-        return (self.rate(rating1, [(WIN, rating2)]),
-                self.rate(rating2, [(LOSS, rating1)]))
+    def _delta(self, rating_list, rd_list, outcome_list, v):
+        temp_sum = 0
+        for i in range(len(rating_list)):
+            temp_sum += _g(rd_list[i]) * (outcome_list[i] - self._e(rating_list[i], rd_list[i]))
+        return v * temp_sum
+
+    def _v(self, rating_list, rd_list):
+        temp_sum = 0
+        for i in range(len(rating_list)):
+            temp_e = self._e(rating_list[i], rd_list[i])
+            temp_sum += math.pow(_g(rd_list[i]), 2) * temp_e * (1 - temp_e)
+        return 1 / temp_sum
+
+    def _e(self, p2rating, p2_rd):
+        return 1 / (1 + math.exp(-1 * _g(p2_rd) * (self.__rating - p2rating)))
