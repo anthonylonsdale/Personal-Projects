b8e4be2b781b6e0f167d616f65efa74af8d65bc5
alons3253 <71347068+alons3253@users.noreply.github.com>
2021-06-15 18:28:20 -0500

    Update CallPricing.cpp

--- C Plus Plus/CallPricingFiles/CallPricing.cpp
+++ C Plus Plus/CallPricingFiles/CallPricing.cpp
@@ -1,8 +1,5 @@
 #include "pch.h"
-#include <iostream>
-#include <cmath>
 #include <vector>
-#include <sstream>
 
 using namespace std;
 
@@ -15,13 +12,13 @@ main entry point for this file is the CallPricing function, which is called as a
 my python program using CTYPEs in order to price options contracts very quickly.
 The intention is to keep this c plus plus file as fast and lean as possible due to the
 magnitude of calculations we have to perform. 1000 iterations seems to strike an optimal
-balance between pricing accuracy and speed.
+balance between pricing accuracy and speed. Despite the name, this file calculates both
+call and put option prices.
 */
 
 DLLEXPORT double CallPricing(double Spot, double Strike, double Rate, double Time, double Sigma, double Yield, int iterations)
 {
     double Option_Price;
-
     double delta, u, d, q;
 
     Time = Time / 365;
@@ -54,7 +51,46 @@ DLLEXPORT double CallPricing(double Spot, double Strike, double Rate, double Tim
     }
     Option_Price = valueTree[0][0];
     //std::cout << "Calculated price using "<<iterations<<" iterations is " << Stock_Price << std::endl;
-    cout << Option_Price;
+    //cout << Option_Price;
+    return Option_Price;
+}
+
+DLLEXPORT double PutPricing(double Spot, double Strike, double Rate, double Time, double Sigma, double Yield, int iterations)
+{
+    double Option_Price;
+    double delta, u, d, q;
+
+    Time = Time / 365;
+    delta = Time / iterations;
+    u = exp(Sigma * sqrt(delta));
+    d = 1 / u;
+    q = (exp((Rate - Yield) * delta) - d) / (u - d);
+    // create storage for the stock price tree and option price tree
+    vector<vector<double>> stockTree(iterations + 1, vector<double>(iterations + 1));
+    // setup and initialise the stock price tree
+    for (int i = 0;i <= iterations;i++)
+    {
+        for (int j = 0;j <= i;j++)
+        {
+            stockTree[i][j] = Spot * pow(u, j) * pow(d, i - j);
+        }
+    }
+    vector<vector<double>> valueTree(iterations + 1, vector<double>(iterations + 1));
+
+    for (int j = 0;j <= iterations;j++)
+    {
+        valueTree[iterations][j] = max(Strike - stockTree[iterations][j], 0.);
+    }
+    for (int i = iterations - 1;i >= 0;i--)
+    {
+        for (int j = 0;j <= i;j++)
+        {
+            valueTree[i][j] = exp(-Rate * delta) * (q * valueTree[i + 1][j + 1] + (1 - q) * valueTree[i + 1][j]);
+        }
+    }
+    Option_Price = valueTree[0][0];
+    //std::cout << "Calculated price using "<<iterations<<" iterations is " << Stock_Price << std::endl;
+    //cout << Option_Price;
     return Option_Price;
 }
 
