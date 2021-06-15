02de9bec8313ab3d6b18f1d0e8406664bfcd4aba
alons3253 <71347068+alons3253@users.noreply.github.com>
2021-06-14 22:37:56 -0500

    Update Trade Executor.cs

--- Algorithmic Trader Project/Trade Executor.cs
+++ Algorithmic Trader Project/Trade Executor.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Alpaca.Markets;
 
@@ -6,29 +8,58 @@ namespace CSharpTradeExecutor
 {
     public class BracketOrders
     {
-        public static async Task Trader(String[] args)
+        public static async Task Trader(string[] array)
         {
-            String symbol = args[1];
-            String side = args[2];
-            int round_lot = int.Parse(args[3]);
-            long stopLossStopPrice = long.Parse(args[4]);
-            long takeProfitLimitPrice = long.Parse(args[5]);
-            long stopLossLimitPrice = long.Parse(args[6]);
+            String symbol = "";
+            String side = "";
+            int round_lot = 0;
+            decimal stopLossStopPrice = 1.0m;
+            decimal takeProfitLimitPrice = 1.0m;
+            decimal stopLossLimitPrice = 1.0m;
 
-            var tradingClient = Environments.Paper.GetAlpacaTradingClient(new SecretKey("PKCPC6RJ84BG84W3PB60", "U1r9Z2QknL9FwAaTztfLl5g1DTxpa5m97qyWCGZ7"));
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i == 0)
+                {
+                    symbol = array[i];
+                }
+                else if (i == 1)
+                {
+                    side = array[i];
+                }
+                else if (i == 2)
+                {
+                    round_lot = int.Parse(array[i]);
+                }
+                else if (i == 3)
+                {
+                    stopLossStopPrice = decimal.Parse(array[i]);
+                }
+                else if (i == 4)
+                {
+                    takeProfitLimitPrice = decimal.Parse(array[i]);
+                }
+                else if (i == 5)
+                {
+                    stopLossLimitPrice = decimal.Parse(array[i]);
+                }
+            }
+
+            var tradingClient = Alpaca.Markets.Environments.Paper.GetAlpacaTradingClient(
+                                new SecretKey("PKCPC6RJ84BG84W3PB60", "U1r9Z2QknL9FwAaTztfLl5g1DTxpa5m97qyWCGZ7"));
 
             if (side == "buy")
             {
-                await tradingClient.PostOrderAsync(MarketOrder.Buy(symbol, round_lot).WithDuration(TimeInForce.Gtc).Bracket(stopLossStopPrice: stopLossStopPrice, takeProfitLimitPrice: takeProfitLimitPrice, stopLossLimitPrice: stopLossLimitPrice));
-
+                await tradingClient.PostOrderAsync(MarketOrder.Buy(symbol, round_lot).WithDuration(TimeInForce.Gtc).
+                                    Bracket(stopLossStopPrice: stopLossStopPrice, takeProfitLimitPrice: takeProfitLimitPrice,
+                                    stopLossLimitPrice: stopLossLimitPrice));
             }
             if (side == "sell")
             {
-                await tradingClient.PostOrderAsync(MarketOrder.Sell(symbol, round_lot).WithDuration(TimeInForce.Gtc).Bracket(stopLossStopPrice: stopLossStopPrice, takeProfitLimitPrice: takeProfitLimitPrice, stopLossLimitPrice: stopLossLimitPrice));
-
+                await tradingClient.PostOrderAsync(MarketOrder.Sell(symbol, round_lot).WithDuration(TimeInForce.Gtc).
+                                    Bracket(stopLossStopPrice: stopLossStopPrice, takeProfitLimitPrice: takeProfitLimitPrice, 
+                                    stopLossLimitPrice: stopLossLimitPrice));
             }
-
-            Console.WriteLine("Trade has been executed");
         }
     }
 }
