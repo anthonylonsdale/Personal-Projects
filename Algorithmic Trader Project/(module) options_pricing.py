74e5cc4cd3b4c566d0be537e8dc6b16d37cd87be
alons3253 <71347068+alons3253@users.noreply.github.com>
2021-06-17 12:53:25 -0500

    Update (module) options_pricing.py

--- Algorithmic Trader Project/(module) options_pricing.py
+++ Algorithmic Trader Project/(module) options_pricing.py
@@ -19,23 +19,22 @@ class Options:
     def thread_marshaller(self):
         with concurrent.futures.ThreadPoolExecutor(max_workers=len(self.stock_tickers)) as executor:
             for stock in self.stock_tickers:
-                future = executor.submit(self.options, stock)
+                executor.submit(self.options, stock)
 
         return self.option_value
 
     def options(self, stock):
         todays_date = dt.datetime.today().date()
-        iterations = 1000
 
         handle = ctypes.cdll.LoadLibrary(r"C:\Users\fabio\source\repos\CallPricingDll\CallPricingDll\x64\Rel"
                                          r"ease\CallPricingDll.dll")
 
-        handle.CallPricing.argtypes = [ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double,
-                                       ctypes.c_double, ctypes.c_double, ctypes.c_int]
+        handle.CallPricing.argtypes = [ctypes.c_float, ctypes.c_float, ctypes.c_float, ctypes.c_float,
+                                       ctypes.c_float, ctypes.c_float]
         handle.CallPricing.restype = ctypes.c_double
 
-        handle.PutPricing.argtypes = [ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double,
-                                      ctypes.c_double, ctypes.c_double, ctypes.c_int]
+        handle.PutPricing.argtypes = [ctypes.c_float, ctypes.c_float, ctypes.c_float, ctypes.c_float,
+                                      ctypes.c_float, ctypes.c_float]
         handle.PutPricing.restype = ctypes.c_double
 
         openpyxl.Workbook().save(f"{stock} Options Data {todays_date}.xlsx")
@@ -47,7 +46,6 @@ class Options:
             i = 0
             yfticker = yf.Ticker(stock)
             expiration_dates = yfticker.options
-
             dividend = self.initial_data[stock][0]
             spot = self.quote_data[stock][0]
 
@@ -55,46 +53,48 @@ class Options:
                 options_chain = yfticker.option_chain(expiry)
                 call_table = options_chain.calls
                 put_table = options_chain.puts
-                call_table.to_excel(writer, sheet_name=f'{stock} Calls {expiry}')
-                put_table.to_excel(writer, sheet_name=f'{stock} Puts {expiry}')
-
-                # 2 is strike, 3 is last price, 9 is open interest and 10 is implied volatility
-                call_vals = call_table[call_table.columns[[2, 3, 9, 10]]].to_numpy()
-                put_vals = put_table[put_table.columns[[2, 3, 9, 10]]].to_numpy()
+                call_table['option_value'] = 0.00
+                put_table['option_value'] = 0.00
 
                 self.option_value[stock].append({expiry: {'overvalued_call_options': 0, 'undervalued_call_options': 0,
-                                                     'overvalued_put_options': 0, 'undervalued_put_options': 0}})
+                                                          'overvalued_put_options': 0, 'undervalued_put_options': 0}})
                 exp = dt.datetime.strptime(expiry, '%Y-%m-%d')
                 time_dt = exp - dt.datetime.today()
-                time_to_expiry = time_dt.days
+                time_to_expiry = time_dt.days + 1
 
-                for index, row in enumerate(call_vals):
-                    sigma = row[3]
+                for index, row in call_table.iterrows():
+                    sigma = row['impliedVolatility']
                     if sigma == 0.00:
                         continue
-                    if row[2] < 10:
+                    if row['openInterest'] < 10:
                         continue
 
-                    strike = row[0]
-                    option_price = handle.CallPricing(spot, strike, self.rate, time_to_expiry, sigma, dividend, iterations)
-                    if option_price > row[1]:
+                    strike = row['strike']
+                    option_price = handle.CallPricing(spot, strike, self.rate, time_to_expiry, sigma, dividend)
+                    call_table.at[index, 'option_value'] = option_price
+
+                    if option_price > row['lastPrice']:
                         self.option_value[stock][i][expiry]['undervalued_call_options'] += 1
-                    if option_price < row[1]:
+                    if option_price < row['lastPrice']:
                         self.option_value[stock][i][expiry]['overvalued_call_options'] += 1
 
-                for index, row in enumerate(put_vals):
-                    sigma = row[3]
+                for index, row in put_table.iterrows():
+                    sigma = row['impliedVolatility']
                     if sigma == 0.00:
                         continue
-                    if row[2] < 10:
+                    if row['openInterest'] < 10:
                         continue
 
-                    strike = row[0]
-                    option_price = handle.PutPricing(spot, strike, self.rate, time_to_expiry, sigma, dividend, iterations)
-                    if option_price > row[1]:
+                    strike = row['strike']
+                    option_price = handle.PutPricing(spot, strike, self.rate, time_to_expiry, sigma, dividend)
+                    put_table.at[index, 'option_value'] = option_price
+
+                    if option_price > row['lastPrice']:
                         self.option_value[stock][i][expiry]['undervalued_put_options'] += 1
-                    if option_price < row[1]:
+                    if option_price < row['lastPrice']:
                         self.option_value[stock][i][expiry]['overvalued_put_options'] += 1
                 i += 1
+                call_table.to_excel(writer, sheet_name=f'{stock} Calls {expiry}')
+                put_table.to_excel(writer, sheet_name=f'{stock} Puts {expiry}')
         finally:
             book.save(f"{stock} Options Data {todays_date}.xlsx")
