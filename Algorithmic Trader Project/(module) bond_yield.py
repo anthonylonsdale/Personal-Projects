5645cbc52d69277f925d5e776732e9c82dccb902
alons3253 <71347068+alons3253@users.noreply.github.com>
2021-06-20 14:14:33 -0500

    Update (module) bond_yield.py

--- Algorithmic Trader Project/(module) bond_yield.py
+++ Algorithmic Trader Project/(module) bond_yield.py
@@ -1,7 +1,9 @@
 from requests import get
 from bs4 import BeautifulSoup
 import pandas as pd
+from pandas.tseries.offsets import BDay
 import openpyxl
+import datetime as dt
 """
 The methodology will be expanded upon later but essentially
 we can take a look at changing bond prices as an indicator
@@ -10,40 +12,56 @@ signals more investors are purchasing bonds, causing
 yields to decrease, and the opposite is true.
 """
 
+class treasuryYields:
+    def __init__(self):
+        self.bond_yields = {}
+        self.todays_date = dt.datetime.today() - BDay(1)
+        self.url = f"C:/Users/fabio/PycharmProjects/AlgoTrader/ALGO/Daily Stock Analysis/Bonds/T-Bonds Data " \
+                   f"{self.todays_date.date()}.xlsx"
+        wb = openpyxl.Workbook()
+        wb.save(self.url)
+        self.r = get("https://www.treasury.gov/resource-center/data-chart-center/interest-rates/Pages/Text"
+                     "View.aspx?data=yield")
 
-def treasury_bond_yields():
-    bond_yields = {}
-    r = get("https://www.treasury.gov/resource-center/data-chart-center/interest-rates/Pages/TextView.aspx?data=yield")
-    soup = BeautifulSoup(r.text, 'lxml')
-    bond_list = [entry.text for entry in soup.find_all('td', {'class': 'text_view_data'})]
+    def treasury_bond_yields(self):
+        soup = BeautifulSoup(self.r.text, 'lxml')
+        bond_list = [entry.text for entry in soup.find_all('td', {'class': 'text_view_data'})]
+        date = None
+        for i in range(len(bond_list)):
+            if i % 13 == 0:
+                date = bond_list[i]
+                self.bond_yields[date] = []
+                continue
+            self.bond_yields[date].append(bond_list[i])
 
-    date = None
-    for i in range(len(bond_list)):
-        if i % 13 == 0:
-            date = bond_list[i]
-            bond_yields[date] = []
-            continue
-        bond_yields[date].append(bond_list[i])
+        bond_df = pd.DataFrame.from_dict(self.bond_yields).T
+        bond_df = bond_df.rename(columns={0: '1 Mo', 1: '2 Mo', 2: '3 Mo', 3: '6 Mo', 4: '1 Yr', 5: '2 Yr',
+                                          6: '3 Yr', 7: '5 Yr', 8: '7 Yr', 9: '10 Yr', 10: '20 Yr', 11: '30 Yr'})
+        string_date = date.replace("/", '-')
+        wb = openpyxl.Workbook()
+        wb.save(self.url)
+        book = openpyxl.load_workbook(self.url)
+        writer = pd.ExcelWriter(self.url, engine='openpyxl')
+        writer.book = book
+        writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
+        bond_df.to_excel(writer, sheet_name=f"Treasury Bonds for {string_date}")
+        try:
+            sheet = book['Sheet']
+            book.remove(sheet)
+        except KeyError:
+            pass
+        book.save(self.url)
 
-    bond_df = pd.DataFrame.from_dict(bond_yields).T
-    bond_df = bond_df.rename(columns={0: '1 Mo', 1: '2 Mo', 2: '3 Mo', 3: '6 Mo', 4: '1 Yr', 5: '2 Yr',
-                                      6: '3 Yr', 7: '5 Yr', 8: '7 Yr', 9: '10 Yr', 10: '20 Yr', 11: '30 Yr'})
-    string_date = date.replace("/", '-')
-    url = f"C:/Users/fabio/PycharmProjects/AlgoTrader/ALGO/Daily Stock Analysis/Bonds/T-Bonds Data {string_date}.xlsx"
-    wb = openpyxl.Workbook()
-    wb.save(url)
-    book = openpyxl.load_workbook(url)
-    writer = pd.ExcelWriter(url, engine='openpyxl')
-    writer.book = book
-    writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
-    bond_df.to_excel(writer, sheet_name=f"Treasury Bonds for {string_date}")
-    try:
-        sheet = book['Sheet']
-        book.remove(sheet)
-    except KeyError:
-        pass
-    book.save(url)
+        onemonthyield = bond_df.iloc[13, 0]
+        twomonthyield = bond_df.iloc[13, 1]
+        threemonthyield = bond_df.iloc[13, 2]
+        sixmonthyield = bond_df.iloc[13, 3]
+        oneyryield = bond_df.iloc[13, 4]
+        twoyryield = bond_df.iloc[13, 5]
+        return (onemonthyield, twomonthyield, threemonthyield,
+                sixmonthyield, oneyryield, twoyryield)
 
 
 if __name__ == "__main__":
-    treasury_bond_yields()
+    bond_object = treasuryYields()
+    bond_object.treasury_bond_yields()
