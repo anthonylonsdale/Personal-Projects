b8b4c32c7e2345952a4290197874d19b89fec991
Anthony <71347068+alons3253@users.noreply.github.com>
2021-10-30 00:36:11 -0500

    Update ExcelFormatting.cs

--- C Sharp Modules/ExcelFormatting.cs
+++ C Sharp Modules/ExcelFormatting.cs
@@ -1,21 +1,21 @@
 using System;
 using Excel = Microsoft.Office.Interop.Excel;
 
+
 namespace Excel_Interop
 {
     public class ExcelFormatting
     {
         public static void ExcelRun(String filename)
         {
-            object m = Type.Missing;
-
-            Excel.Application excelApp = new Excel.Application();
-            excelApp.Visible = false;
-            excelApp.DisplayAlerts = false;
+            Excel.Application excelApp = new Excel.Application
+            {
+                Visible = false,
+                DisplayAlerts = false
+            };
 
             Excel.Workbooks xlWorkbooks = excelApp.Workbooks;
             Excel.Workbook xlWorkbook = xlWorkbooks.Open(filename);
-            //Excel.Worksheet xlWorksheet = (Excel.Worksheet)xlWorkbook.Worksheets[wks];
 
             foreach ( Excel.Worksheet worksheet in xlWorkbook.Worksheets )
             {
@@ -23,25 +23,21 @@ namespace Excel_Interop
                 worksheet.Rows.AutoFit();
             }
 
-            //xlWorksheet.Columns.AutoFit();
-            //xlWorksheet.Rows.AutoFit();
-
+            
             xlWorkbook.Close(SaveChanges: true, Filename: filename);
             excelApp.Quit();
 
             GC.Collect();
             GC.WaitForPendingFinalizers();
 
-            xlWorkbooks = null;
-            xlWorkbook = null;
-            //worksheet = null;
-            excelApp = null;
+            System.Runtime.InteropServices.Marshal.FinalReleaseComObject(xlWorkbooks);
+            System.Runtime.InteropServices.Marshal.FinalReleaseComObject(xlWorkbook);
+            System.Runtime.InteropServices.Marshal.FinalReleaseComObject(excelApp);
         }
 
-        public static void Main(String[] args)
+        public static void Main(String args)
         {
-            String filename = args[0];
-            //String wks = args[1];
+            String filename = args;
             ExcelRun(filename);
         }
     }
