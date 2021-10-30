7715abff5fe360746634e70f664ef347aea623bd
Anthony <71347068+alons3253@users.noreply.github.com>
2021-10-30 16:52:20 -0500

    Update CallPricing.cpp

--- C Plus Plus/CallPricingFiles/CallPricing.cpp
+++ C Plus Plus/CallPricingFiles/CallPricing.cpp
@@ -1,6 +1,6 @@
 #include "pch.h"
 #include <iostream>
-#include <algorithm>
+#include <vector>
 
 #define DLLEXPORT extern "C" __declspec(dllexport)
 
@@ -10,98 +10,86 @@ compiled using x64 architecture and only x64
 main entry point for this file is the CallPricing function, which is called as a DLL from
 my python program using CTYPEs in order to price options contracts very quickly.
 The intention is to keep this c plus plus file as fast and lean as possible due to the
-magnitude of calculations we have to perform. 1000 iterations seems to strike an optimal
+magnitude of calculations we have to perform. 500 iterations seems to strike an optimal
 balance between pricing accuracy and speed.
 */
 
 DLLEXPORT double CallPricing(float s, float k, float rf, float t, float v)
 {
-    double Option_Price, u, d, q, rfr;
-
-    // 1 divided by 365 is .00273973, delta is again divided by the size of the array giving us .000027397
-    const double divisor = 0.00273973;
-
-    const int n = 500;
-
-    const double delta = t * divisor / n;
-
-    u = std::exp(v * std::sqrt(delta));
-    d = 1 / u;
-    rfr = std::exp(rf * delta);
-    q = (rfr - d) / (u - d);
-
-    auto* stockTree = new double[n+1][n+1];
-    for (int i = 0;i <= n;i++)
-    {
-        for (int j = 0;j <= i;j++)
-        {
-            stockTree[i][j] = s * std::pow(u, 2 * j - i);
-        }
-    }
-    auto* valueTree = new double[n+1][n+1];
-    for (int j = 0;j <= n;j++)
-    {
-        valueTree[n][j] = max(stockTree[n][j] - k, 0.);
-    }
-    delete[]stockTree;
-    for (int i = n-1;i >= 0;i--)
-    {
-        for (int j = 0;j <= i;j++)
-        {
-            valueTree[i][j] = rfr * (q * valueTree[i + 1][j + 1] + (1 - q) * valueTree[i + 1][j]);
-        }
-    }
-    Option_Price = valueTree[0][0];
-    delete[]valueTree;
-    return Option_Price;
-}
-
-DLLEXPORT double PutPricing(double s, double k, double rf, double t, double v)
-{
-    double h, u, d, drift, q;
+    double h, u, d;
     // the divisor is 1 / 365, the division is already computed
-    const double divisor = 0.00273973;
-    int i;
-    const int n = 100;
+    const double divisor = 0.002738;
+    int i, j;
+    const int n = 500;
     auto* stkval = new double[n + 1][n + 1];
 
-    h = t * .01 * divisor;
-    u = std::exp((rf - (0.5 * v * v)) * h + (v * std::sqrt(h)));
-    d = std::exp((rf - (0.5 * v * v)) * h - (v * std::sqrt(h)));
-    drift = std::exp(rf * h);
-    q = (drift - d) / (u - d);
+    h = t * .002 * divisor;
+    u = std::exp(v * sqrt(2 * h));
+    d = 1 / u;
+
+    double pu = pow((exp(0.5 * rf * h) - exp(-v * sqrt(0.5 * h))) / (exp(v * sqrt(0.5 * h)) - exp(-v * sqrt(0.5 * h))), 2);
+    double pd = pow((exp(v * sqrt(0.5 * h)) - exp(0.5 * rf * h)) / (exp(v * sqrt(0.5 * h)) - exp(-v * sqrt(0.5 * h))), 2);
+    double pm = 1.0 - pu - pd;
+
+    //q = (drift - d) / (u - d);
 
     // processing terminal stock price
-    stkval[0][0] = s;
-    for (int i = 1; i < n + 1; i++)
-    {
-        stkval[i][0] = stkval[i - 1][0] * u;
-        for (int j = 1; j < i + 1; j++)
-        {
-            stkval[i][j] = stkval[i - 1][j - 1] * d;
-        }
+    std::vector<std::vector<double> > S(2 * n + 1, std::vector<double>(n + 1));
+    for (j = 0; j <= n; j++)
+        for (i = 0; i <= 2 * j; i++)
+            S[i][j] = s * pow(u, double(j - i));
+
+    std::vector<std::vector<double> > V(2 * n + 1, std::vector<double>(n + 1));
+    // Compute terminal payoffs
+    for (i = 0; i <= 2 * n; i++) {
+        V[i][n] = max(S[i][n] - k, 0.0);
     }
 
-    // backward recursion to obtain option_price
-    auto* optval = new double[n + 1][n + 1];
-    for (int j = 0; j < n + 1; j++)
-    {
-        optval[n][j] = max(0, k - stkval[n][j]);
-    }
-    for (int m = 0; m < n; m++)
-    {
-        i = n - m - 1;
-        for (int j = 0; j < i + 1; j++)
-        {
-            optval[i][j] = (q * optval[i + 1][j] + (1 - q) * optval[i + 1][j + 1]) / drift;
-            // check for early exercise
-            optval[i][j] = max(optval[i][j], k - stkval[i][j]);
+    for (j = n - 1; j >= 0; j--) {
+        for (i = 0; i <= 2 * j; i++) {
+             V[i][j] = max(S[i][j] - k, exp(-rf * h) * (pu * V[i][j + 1] + pm * V[i + 1][j + 1] + pd * V[i + 2][j + 1]));
         }
     }
-    double option_price = optval[0][0];
-    delete[]stkval;
-    delete[]optval;
-    return option_price;
+    return V[0][0];
+}
+
+DLLEXPORT double PutPricing(float s, float k, float rf, float t, float v)
+{
+    double h, u, d;
+    // the divisor is 1 / 365, the division is already computed
+    const double divisor = 0.002738;
+    int i, j;
+    const int n = 500;
+    auto* stkval = new double[n + 1][n + 1];
+
+    h = t * .002 * divisor;
+    u = std::exp(v * sqrt(2 * h));
+    d = 1 / u;
+
+    double pu = pow((exp(0.5 * rf * h) - exp(-v * sqrt(0.5 * h))) / (exp(v * sqrt(0.5 * h)) - exp(-v * sqrt(0.5 * h))), 2);
+    double pd = pow((exp(v * sqrt(0.5 * h)) - exp(0.5 * rf * h)) / (exp(v * sqrt(0.5 * h)) - exp(-v * sqrt(0.5 * h))), 2);
+    double pm = 1.0 - pu - pd;
+
+    //q = (drift - d) / (u - d);
+
+    // processing terminal stock price
+    std::vector<std::vector<double> > S(2 * n + 1, std::vector<double>(n + 1));
+    for (j = 0; j <= n; j++)
+        for (i = 0; i <= 2 * j; i++)
+            S[i][j] = s * pow(u, double(j - i));
+
+    std::vector<std::vector<double> > V(2 * n + 1, std::vector<double>(n + 1));
+    // Compute terminal payoffs
+    for (i = 0; i <= 2 * n; i++) {
+        V[i][n] = max(k - S[i][n], 0.0);
+    }
+
+    for (j = n - 1; j >= 0; j--) {
+        for (i = 0; i <= 2 * j; i++) {
+            V[i][j] = max(k - S[i][j], exp(-rf * h) * (pu * V[i][j + 1] + pm * V[i + 1][j + 1] + pd * V[i + 2][j + 1]));
+        }
+    }
+    return V[0][0];
 }
 
 int main(int argc, char** argv)
